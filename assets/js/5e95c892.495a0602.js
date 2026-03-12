9f530c9848f2444b5d23a85233d8163b82796d39
ahmetbilgay <ahmetbilgay@users.noreply.github.com>
2026-03-12 22:56:39 +0000

    deploy: 858db4e0f2004d5d4c51cc896f2110c1f355033d

---
+++ assets/js/5e95c892.495a0602.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkparevo_core_docs=globalThis.webpackChunkparevo_core_docs||[]).push([[647],{7121(e,r,s){s.r(r),s.d(r,{default:()=>h});s(6540);var a=s(4164),c=s(7559),o=s(5500),u=s(2831),l=s(1656),d=s(4848);function h(e){return(0,d.jsx)(o.e3,{className:(0,a.A)(c.G.wrapper.docsPages),children:(0,d.jsx)(l.A,{children:(0,u.v)(e.route.routes)})})}}}]);
---
+++ assets/js/aba21aa0.fe014150.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkparevo_core_docs=globalThis.webpackChunkparevo_core_docs||[]).push([[742],{7093(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
